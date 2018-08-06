1058a8342cf66309107711a31372c1c822cb90d0
Mat <lumierematerne11@gmail.com>
2018-08-06 18:39:49 +0100

    Setup handlebars for backend

--- server/app.js
+++ server/app.js
@@ -2,9 +2,20 @@ const SERVER_PORT = process.env.PORT || 4000;
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const exphbs = require("express-handlebars");
+
+app.engine(
+  "hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: "hbs"
+  })
+);
+app.set("view engine", "hbs");
 
 app.use(cors());
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/admin/drivers", (req, res) => res.render("drivers"));
 
 app.get("/api/status", (req, res) => {
   res.send({
