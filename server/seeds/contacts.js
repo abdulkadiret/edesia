261b1e604010867978710cc34f2859d44edea695
Abdulkadir <ethiogentelman@gmail.com>
2018-08-17 17:42:17 -0400

    change to one migration file

--- server/seeds/contacts.js
+++
@@ -1,9 +0,0 @@
-exports.seed = function (knex, Promise) {
-    // Deletes ALL existing entries
-    return knex("contacts")
-        .del()
-        .then(function () {
-            // Inserts seed entries
-            return knex("contacts").insert([{ name: "Maj" , phone: "073....."}, { name: "Nasir", phone: "074...." }]);
-        });
-};
