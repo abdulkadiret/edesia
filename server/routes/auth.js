303b1ba17d8f0336eb603c86d8e74a51238a137f
Awet Fessha <awetkifle94@gmail.com>
2018-08-19 13:40:19 +0100

    assign jwt secret to the envt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -17,7 +17,7 @@ router.post("/login", function(req, res, next) {
         res.send(err);
       }
       // generate a signed son web token with the contents of user object and return it in the response
-      const token = jwt.sign(user, "your_jwt_secret");
+      const token = jwt.sign(user, process.env.JWT_SECRET || "your_jwt_secret");
       return res.json({ user, token });
     });
   })(req, res);
