c3322e4c8b95e241e37cea40ad4ac582012b5605
Awet Fessha <awetkifle94@gmail.com>
2018-08-30 23:01:08 +0100

    import Deliveries admin and insert it to Deliveries

--- client/src/containers/Deliveries/Deliveries.js
+++ client/src/containers/Deliveries/Deliveries.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import DeliveriesAdmin from "../../containers/DeliveriesAdmin/DeliveriesAdmin";
 
 class Deliveries extends Component {
   render() {
-    return <div className="App">Deliveries page ....</div>;
+    return (
+      <div className="App">
+        <DeliveriesAdmin />
+      </div>
+    );
   }
 }
 
