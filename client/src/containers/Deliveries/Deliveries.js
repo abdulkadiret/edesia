93529abe7d668e137de0745b51cd41469dfea019
Awet Fessha <awetkifle94@gmail.com>
2018-08-31 17:54:15 +0100

    remove DeliveriesAdmin from deliveries page

--- client/src/containers/Deliveries/Deliveries.js
+++ client/src/containers/Deliveries/Deliveries.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
-import DeliveriesAdmin from "../../containers/DeliveriesAdmin/DeliveriesAdmin";
 
 class Deliveries extends Component {
   render() {
     return (
       <div className="App">
         <h2>Deliveries </h2>
-        <DeliveriesAdmin />
       </div>
     );
   }
