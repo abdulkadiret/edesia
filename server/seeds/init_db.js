89e93269606a2580c4a663e4db392165fd068286
Awet K Fessha <31638686+AKFessha@users.noreply.github.com>
2018-08-31 23:10:50 +0100

    Change date format to PostgreSQL date format (#37)
    * change date format to american
    
    * change date to postgress date format

--- server/seeds/init_db.js
+++ server/seeds/init_db.js
@@ -48,8 +48,8 @@ exports.seed = async (knex, Promise) => {
     .then(function() {
       // Inserts seed entries
       return knex("deliveries").insert([
-        { address: "Govan", deadline: "24/09/2018" },
-        { address: "Parkhead", deadline: "12/10/2018" }
+        { address: "Govan", deadline: "2018-09-24" },
+        { address: "Parkhead", deadline: "2018-10-12" }
       ]);
     });
   await knex("status")
