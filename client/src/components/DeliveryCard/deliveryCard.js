b65f8c665c709dfe96a474f9d53dda10db720646
Abdulkadir <ethiogentelman@gmail.com>
2018-09-01 16:06:32 -0400

    create deliveryCard.js and deliveryCard.css

---
+++ client/src/components/DeliveryCard/deliveryCard.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./deliveryCard.css"
+import { Link } from "react-router-dom";
+
+const deliveryCard = props => (
+  <div className="deliveryCard col-sm-12 col-md-3 ">
+    <h3>
+      <Link to="/deliveries/:delivery_id">{props.storeName}</Link>
+    </h3>
+  </div>
+);
+
+export default deliveryCard;
