e3949c9a934f2f84f010b00191598023192163c3
Abdulkadir <ethiogentelman@gmail.com>
2018-09-08 15:31:42 -0400

    add password for server getUserProfile

--- server/helpers/db.js
+++ server/helpers/db.js
@@ -17,7 +17,7 @@ const getSingleUser = (email, password) => {
 };
 const getUserProfile = userId => {
   return knex("users")
-    .select("user_id", "email", "name", "postcode", "city")
+    .select("user_id", "email", "name", "postcode", "city", "password")
     .where({ user_id: userId })
     .first();
 };
@@ -53,7 +53,8 @@ const editUserProfile = (user_id, data) => {
       name: data.name,
       email: data.email,
       city: data.city,
-      postcode: data.postcode
+      postcode: data.postcode,
+      password:data.password,
     });
 };
 module.exports = {
